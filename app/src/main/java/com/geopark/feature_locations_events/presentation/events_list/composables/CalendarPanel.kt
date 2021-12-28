82915ee7dc53579ad595c11e60974d488ddd789f
Miko <mikics2k17@gmail.com>
2021-12-28 14:43:41 +0100

    In CalendarPanel.kt previous day are shown now. When panel is in current month, scroll position is set to current day else initial scroll position is set to first day of the month.

--- app/src/main/java/com/geopark/feature_locations_events/presentation/events_list/composables/CalendarPanel.kt
+++ app/src/main/java/com/geopark/feature_locations_events/presentation/events_list/composables/CalendarPanel.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,9 +21,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.geopark.core.util.getShortName
 import com.geopark.feature_locations_events.presentation.events_list.CalendarPanelState
-
 import com.geopark.ui.theme.DirtyWhite
 import com.geopark.ui.theme.NavyBlue
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 @Composable
@@ -36,43 +38,58 @@ fun CalendarPanel(
             .height(150.dp)
             .background(DirtyWhite)
     ) {
+
+        val dayRowState = rememberLazyListState(data.currentDay)
+        val coroutineScope = rememberCoroutineScope()
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically
         ) {
-                IconButton(onClick = {
-                    onMonthChange(-1)
-                }) {
-                    Icon(Icons.Filled.ArrowBack, "Previous month")
+            IconButton(onClick = {
+                onMonthChange(-1)
+                coroutineScope.launch {
+                    dayRowState.animateScrollToItem(if (data.month.minus(1) == LocalDate.now().month) LocalDate.now().dayOfMonth else 0)
                 }
+            }) {
+                Icon(Icons.Filled.ArrowBack, "Previous month")
 
+            }
 
             Text("${data.month.name}, ${data.year}", style = MaterialTheme.typography.h6)
             IconButton(onClick = {
                 onMonthChange(1)
+                coroutineScope.launch {
+                    dayRowState.animateScrollToItem(if (data.month.plus(1) == LocalDate.now().month) LocalDate.now().dayOfMonth else 0)
+                }
             }) {
                 Icon(Icons.Filled.ArrowForward, "Next month")
             }
         }
+
         LazyRow(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
+            contentPadding = PaddingValues(horizontal = 6.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            state = dayRowState
         ) {
 
-            items(data.numberOfDays) { i ->
+            items(data.daysInMonth) { i ->
                 CalendarDayItem(
-                    dayName = LocalDate.of(data.year, data.month, data.currentDay + i).dayOfWeek.getShortName(), //or get name here
-                    dayNumber = data.currentDay + i,
-                    isSelected = data.selectedDayOfMonth == i + data.currentDay
+                    dayName = LocalDate.of(
+                        data.year,
+                        data.month,
+                        i + 1
+                    ).dayOfWeek.getShortName(), //or get name here
+                    dayNumber = i + 1,
+                    isSelected = data.selectedDayOfMonth == i + 1
                 ) {
-                    onDayChange(data.currentDay + i)
+                    onDayChange(i + 1)
                 }
             }
         }
     }
 }
-
 
 @Composable
 fun CalendarDayItem(dayName: String, dayNumber: Int, isSelected: Boolean, onSelect: () -> Unit) {
